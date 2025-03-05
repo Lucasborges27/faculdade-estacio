72ad91bb5e835eababd67ccc50a0badbada36171
Lucasborges27 <lucasjapa5741@gmail.com>
2025-03-05 22:45:06 +0000

    primeiro commit

---
+++ supertrunfo.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+
+    /*
+%d: Imprime um inteiro no formato decimal.
+%i: Equivalente a %d.
+%f: Imprime um número de ponto flutuante no formato padrão.
+%e: Imprime um número de ponto flutuante na notação científica.
+%c: Imprime um único caractere.
+%s: Imprime uma cadeia (string) de caracteres.
+*/ 
+
+
+int main(){
+printf("Bem vindo ao Super Trunfo\n");
+printf("vamos coletar alguns dados da Carta 1 okay?\n");
+scanf("DIGITE SEU NOME");
+
+
+    
+}
