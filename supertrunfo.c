1be05c5fbce38ecbbb92f9227f5a93df41a0ddd9
Lucasborges27 <lucasjapa5741@gmail.com>
2025-03-11 22:23:47 +0000

    FINALIZADO CORRIGIDO

--- supertrunfo.c
+++ supertrunfo.c
@@ -23,12 +23,12 @@ float area2, pib2;
 printf("Bem vindo ao Super Trunfo\n");
 printf("vamos coletar alguns dados da Carta 1 okay?\n");
 printf("Digite o ESTADO\n");
-fgets(estado,50,stdin); //USADO O FGETS PARA ESTADOS OU CIDADES COM MAIS DE UMA PALAVRA
+fgets(estado,50,stdin); //USADO O FGETS (LEITURA DA LINHA INTEIRA) PARA ESTADOS OU CIDADES COM MAIS DE UMA PALAVRA
 printf("Digite o CODIGO da carta (INICIAL DO ESTADO + NUMERO DE 01 A 04)\n");
-scanf(" %3s", &codigo);
-getchar(); //limpar o \n do meu fgets para não ocupar o buffer do scanf
+scanf(" %3s", &codigo); // USEI O 3 PARA DEFINIR QUE SÓ PODERIA RECEBER 3 ESPAÇOS (LETRA + 01 A 04)
+getchar(); // USEI PARA "LIMPAR" ESPAÇO NÃO PREENCHIDO NO MEU \N POIS VI QUE SEM ISSO ESTAVA PULANDO AS PERGUNTAS
 printf("Digite o NOME DA CIDADE\n");
-fgets(nomedacidade,50,stdin);
+fgets(nomedacidade,50,stdin); //USADO O FGETS (LEITURA DA LINHA INTEIRA) PARA ESTADOS OU CIDADES COM MAIS DE UMA PALAVRA
 printf("Digite a POPULAÇÃO da cidade \n");
 scanf(" %d",&populacao);
 printf("Digite a ÁREA TERRITORIAL (Km2)\n");
@@ -38,16 +38,20 @@ scanf("%f",&pib);
 printf("Digite o número de pontos turísticos\n");
 scanf("%d",&turisticos);
 
+system("clear");
+
+
 
 //COLETANDO DADOS DA CARTA 2
 printf("Ótimo! Agora vamos coletar alguns dados da Carta 2, pode ser?\n");
 printf("Digite o ESTADO\n");
-fgets(estado2,50,stdin); //USADO O FGETS PARA ESTADOS OU CIDADES COM MAIS DE UMA PALAVRA
+getchar(); // USEI PARA "LIMPAR" ESPAÇO NÃO PREENCHIDO NO MEU \N POIS VI QUE SEM ISSO ESTAVA PULANDO AS PERGUNTAS
+fgets(estado2,50,stdin); //USADO O FGETS (LEITURA DA LINHA INTEIRA) PARA ESTADOS OU CIDADES COM MAIS DE UMA PALAVRA
 printf("Digite o CODIGO da carta (INICIAL DO ESTADO + NUMERO DE 01 A 04)\n");
-scanf(" %3s", &codigo2);
-getchar(); //limpar o \n do meu fgets para não ocupar o buffer do scanf
+scanf(" %3s", &codigo2); // USEI O 3 PARA DEFINIR QUE SÓ PODERIA RECEBER 3 ESPAÇOS (LETRA + 01 A 04)
 printf("Digite o NOME DA CIDADE\n");
-fgets(nomedacidade2,50,stdin);
+getchar();
+fgets(nomedacidade2,50,stdin); //USADO O FGETS (LEITURA DA LINHA INTEIRA) PARA ESTADOS OU CIDADES COM MAIS DE UMA PALAVRA
 printf("Digite a POPULAÇÃO da cidade \n");
 scanf(" %d",&populacao2);
 printf("Digite a ÁREA TERRITORIAL (Km2)\n");
@@ -57,36 +61,36 @@ scanf("%f",&pib2);
 printf("Digite o número de pontos turísticos\n");
 scanf("%d",&turisticos2);
 
+system("clear"); // LIMPAR O TERMINAL APÓS PREENCHER TODOS OS DADOS DAS CARTAS MAS MANTENDO OS DADOS ARMAZENADOS
+
 
 
 //APÓS A COLETA DOS DADOS DA CARTA 1 VAMOS A IMPRESSÃO PARA O USUARIO
 
-printf("ESSA É A NOSSA CARTA 1:\n");
+printf("ESSA É A NOSSA CARTA 1.\n");
 estado[strcspn(estado, "\n")] = 0;
 printf("Estado: %s\n", estado);
 printf("Código: %s\n", codigo);
 nomedacidade[strcspn(nomedacidade, "\n")] = 0;
 printf("Nome da Cidade: %s\n", nomedacidade);
 printf("População: %d\n" , populacao);
-printf("Área: %.2f\n", area);
-printf("PIB: %.2f\n", pib);
+printf("Área: %.2f KM²\n", area);
+printf("PIB: %.2f Bilhões de Reais\n", pib);
 printf("Número de Pontos Turísticos: %d\n", turisticos);
+
+printf("\n\n\n"); //APENAS PARA DAR UM ESPAÇO ENTRE A IMPRESSÃO DAS CARTAS NO TERMINAL
+
 
 
 //APÓS A COLETA DOS DADOS DA CARTA 2 VAMOS A IMPRESSÃO PARA O USUARIO
-printf("AGORA VAMOS PARA A CARTA 2!!! \n");
-printf("ESSA É A NOSSA CARTA 2:\n");
+printf("ESSA É A CARTA 2. \n");
 estado2[strcspn(estado2, "\n")] = 0;
 printf("Estado: %s\n", estado2);
-printf("Código: %s\n", codigo2);
+printf("Código da carta: %s\n", codigo2);
 nomedacidade2[strcspn(nomedacidade2, "\n")] = 0;
 printf("Nome da Cidade: %s\n", nomedacidade2);
 printf("População: %d\n", populacao2);
-printf("Área: %.2f\n", area2);
-printf("PIB: %.2f\n", pib2);
+printf("Área: %.2f KM²\n", area2);
+printf("PIB: %.2f Bilhões de Reais\n", pib2);
 printf("Número de Pontos Turísticos: %d\n", turisticos2);
-
-
-
-
 }
